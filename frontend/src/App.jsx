50b7ed8f555869256ff25fb7cfa7fcfe583cdcd0
pronajitdey <deypronajit@gmail.com>
2024-03-17 19:44:12 +0530

    CRUD added in client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,9 +11,6 @@ import { Toaster } from "react-hot-toast";
 
 function App() {
   const { loading, data, error } = useQuery(GET_AUTHENTICATED_USER);
-  console.log("Loading", loading);
-  console.log("Authenticated user:", data);
-  console.log("Error:", error);
 
   if (loading) return null;
 
